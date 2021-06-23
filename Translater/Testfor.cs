b19fe770f34d02b4fab7bce5d34db2d41088867c
GokalpSen <gokalpsen09@gmail.com>
2021-06-23 15:14:06 +0300

    rr

---
+++ Translater/Testfor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Translater
+{
+    class Testfor
+    {
+        public static string Testforfile (string path)
+        {
+            try
+            {
+                File.ReadAllText(path);
+                return "exists";
+            }
+            catch (Exception ex)
+            {
+                return "dont exists";
+            }
+            finally
+            {
+
+            }
+        }
+    }
+}
